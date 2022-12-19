bdfe192c115e60878e7575c1505f41915fe4d431
MiguelPeralta123 <maikperalta123@gmail.com>
2022-12-19 10:14:14 -0700

    login funcional

--- login.js
+++ login.js
@@ -1,11 +1,31 @@
 const url = "https://lacheveriaapi-production.up.railway.app/login"
 
+var uName = ""
+var uPass = ""
+
 fetch(url)
   .then(response => response.json())
   .then(data => {
-    console.log(data)
+    uName = data[0].username
+    uPass = data[0].password
   })
   .catch(err => console.log(err))
+
+const username = document.getElementById("username");
+const password = document.getElementById("password");
+const button = document.getElementById("button");
+
+button.addEventListener("click", (e) => {
+  e.preventDefault();
+  const data = {
+    username: username.value,
+    password: password.value,
+  };
+
+  if (username.value === uName && password.value === uPass) {
+    window.open("file:///C:/Users/Ernesto/Desktop/Miguel%20Peralta/la_cheveria/index.html", "_self");
+  }
+});
 
 /*const mysql = require("mysql")
 
