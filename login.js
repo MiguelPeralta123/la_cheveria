c784090291620fddf713f7e7002b5120618677ae
MiguelPeralta123 <maikperalta123@gmail.com>
2022-12-19 10:34:53 -0700

    login se hace index para abrir primero

--- login.js
+++ login.js
@@ -23,34 +23,6 @@ button.addEventListener("click", (e) => {
   };
 
   if (username.value === uName && password.value === uPass) {
-    window.open("file:///C:/Users/Ernesto/Desktop/Miguel%20Peralta/la_cheveria/index.html", "_self");
+    window.open("file:///C:/Users/Ernesto/Desktop/Miguel%20Peralta/la_cheveria/main.html", "_self");
   }
 });
-
-/*const mysql = require("mysql")
-
-const connection = mysql.createConnection({
-  host: "localhost",
-  user: "sa",
-  password: "123",
-  database: "la_cheveria,",
-});
-connection.connect((err) => {
-  if (err) throw err;
-  console.log("La conexion funciona");
-});
-
-const username = document.getElementById("username");
-const password = document.getElementById("password");
-const button = document.getElementById("button");
-
-button.addEventListener("click", (e) => {
-  e.preventDefault();
-  const data = {
-    username: username.value,
-    password: password.value,
-  };
-
-  console.log(data);
-});
-*/
